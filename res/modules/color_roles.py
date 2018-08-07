d3859ea5ce4548fd1ee5b9484863fa239896fdd2
chahzou <chahzou.aethyra@gmail.com>
2018-08-07 13:45:51 +0200

    Fixed color-roles module to use bot utility

--- res/modules/color_roles.py
+++ res/modules/color_roles.py
@@ -16,7 +16,7 @@ class ColorRoles(Module):
 
         if message is not None:
             member = message.author
-            color_hex_input = await self.client.get_content_part(message.content, 2, 2)
+            color_hex_input = await self.bot.get_content_part(message.content, 2, 2)
 
             if color_hex_input is not None:
                 if re.match(regex, color_hex_input):      # check if given argument is hex code, otherwise send help message
@@ -25,7 +25,7 @@ class ColorRoles(Module):
 
                     for role in member.roles:                   # Uncheck other color roles with name "#hexcode"
                         if re.match(regex, role.name):
-                            await self.client.remove_roles(member, role)
+                            await self.bot.remove_roles(member, role)
 
                     color_hex_raw = int(color_hex[1:], 16)
                     role_to_assign = None
@@ -43,26 +43,26 @@ class ColorRoles(Module):
                             role_to_assign = role
 
                     if role_to_assign is None:                              # Otherwise create new role
-                        role_to_assign = await self.client.create_role(message.server, name=color_hex,
+                        role_to_assign = await self.bot.create_role(message.server, name=color_hex,
                                                                 colour=discord.Colour(color_hex_raw))
 
                     # Use position beneath "bot" role position and add to message author
-                    await self.client.move_role(message.server, role_to_assign, position)
+                    await self.bot.move_role(message.server, role_to_assign, position)
 
-                    await self.client.add_roles(member, role_to_assign)
+                    await self.bot.add_roles(member, role_to_assign)
                     await asyncio.sleep(1)                      # Wait 1 second to finish assigning role
                     # TODO: Guaranteed solution?
 
                     if role_to_assign in member.roles:
 
-                        await self.client.send_message(message.channel, "Color assigned: "+color_hex)
+                        await self.bot.send_message(message.channel, "Color assigned: "+color_hex)
                         print("Color assigned in ", message.server.name)
 
                         await self.delete_unused_color_roles(message)
                 else:
-                    await self.client.util.help_message(message.channel, 'color')
+                    await self.bot.util.help_message(message.channel, 'color')
             else:
-                await self.client.util.help_message(message.channel, 'color')
+                await self.bot.util.help_message(message.channel, 'color')
 
 
 
@@ -84,7 +84,8 @@ class ColorRoles(Module):
                                 used = True                             # when a member has the role -> role is used
 
                     if used is False:
-                        await self.client.delete_role(message.server, role)      # delete the role if it's not used
+                        await self.bot.delete_role(message.server, role)      # delete the role if it's not used
     
+
     async def return_help(self):
-        pass
+        return "Use `color [hex-code]` to set your username color, for example #123456."
