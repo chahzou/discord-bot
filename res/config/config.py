d6a8afdf9a5116a2a8f3f45bc03933f773ff73f9
chahzou <chahzou.aethyra@gmail.com>
2018-10-03 13:33:53 +0200

    Generalized config in dev branch

--- res/config/config.py
+++ res/config/config.py
@@ -1,8 +1,8 @@
 # Configuration
 general = {
     'cmd_op': '!',
-    'name': "Chahzou's bot",
-    'def_channel_id': '407998767233761282',
+    'name': "",
+    'def_channel_id': '',
 }
 
 
@@ -18,11 +18,11 @@ modules = [
 
 # Other
 other = {
-    'ready_msg_toggle': False,
+    'ready_msg_toggle': True,
     'ready_msg': "Now online.",
     'max_msg_len': 10000,
     'max_args': 100,
-    'auto_delete_msgs_channel_ids': ['422045325675593728'],
+    'auto_delete_msgs_channel_ids': [''],
     'auto_delete_delay_s': 10,
-    'clear_channel_ids': ['422045325675593728'],
+    'clear_channel_ids': [''],
 }
