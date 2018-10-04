d982751597e59046afbe91caba65768e8e2988f3
chahzou <chahzou.aethyra@gmail.com>
2018-10-04 23:47:20 +0200

    Added possibility for multiple registration roles, Added error catching

--- res/modules/registration.py
+++ res/modules/registration.py
@@ -1,11 +1,11 @@
 from ..module import Module
 
-
 # Gives user a role and sends a welcome message
+# TODO: Ability to assign registration roles to other users with command
 class Registration(Module):
 
     cmd_arg = 'board'
-    registration_role = 'on-board'
+    registration_roles = ['on-board', 'passengers']
     auto_delete_cmd = False     # Deactivated because auto-delete is already channel-wide
 
     async def run(self, args=None, message=None):
@@ -13,17 +13,25 @@ class Registration(Module):
         if message:
             if args:
                 self.bot.run_module('help', [self.cmd_arg], message)
-                # TODO: Ability to assign registration role to other users
             else:
                 user = message.author
-                # user_name = str(user.name) + "#" + str(user.discriminator)
-                role = await self.bot.util.get_role_by_name(message.server, self.registration_role)
-                def_channel = await self.bot.util.get_default_channel()
 
-                await self.bot.add_roles(user, role)    # Add role
-                
-                await self.bot.util.print(user.name + " has been registered.")
-                await self.bot.send_message(def_channel, "Welcome on board, " + user.mention + ". See <#452838050419048448> for information on the server.")   # Send welcome message
+                try:
+                    for registration_role in self.registration_roles:
+                        role = await self.bot.util.get_role_by_name(message.server, registration_role)
+                        def_channel = await self.bot.util.get_default_channel()
+                        await self.bot.add_roles(user, role)    # Add role
+                        
+                    await self.bot.util.print(user.name + " has been registered.")
+
+                    # Send welcome message
+                    await self.bot.send_message(def_channel, "Welcome on-board, " + user.mention + ". See <#452838050419048448> for information on the server.")
+
+                except Exception as e:
+                    print("Couldn't finish registration. Registration roles: ")
+                    print(*self.registration_roles, sep = ", ")
+                    print(e)
+                    await self.bot.util.send_error_message(message.channel, "Couldn't complete registration.")
 
                 if self.auto_delete_cmd:
                     await self.bot.run_module('mgmt', ['delete', 'last', '1', user.id, 'silent'], message)   # Delete command for this action
