d37555902e675a58d494318cb6e72c1fcb28cad7
chahzou <chahzou.aethyra@gmail.com>
2018-10-05 14:37:42 +0200

    Improved configurability of welcome message in registration

--- res/modules/registration.py
+++ res/modules/registration.py
@@ -6,7 +6,8 @@ class Registration(Module):
 
     cmd_arg = 'board'
     registration_roles = ['on-board', 'passengers']
-    about_channel_id = '452838050419048448'
+    welcome_msg = "Welcome"     # Is followed by ", [user-mention]." and the about message.
+    about_msg = "See <#497747060880048149> for information on the server."
     auto_delete_cmd = False     # Deactivated because auto-delete is already channel-wide
 
     async def run(self, args=None, message=None):
@@ -25,8 +26,9 @@ class Registration(Module):
                         
                     await self.bot.util.print(user.name + " has been registered.")
 
-                    # Send welcome message
-                    await self.bot.send_message(def_channel, "Welcome on-board, " + user.mention + ". See <#" + self.about_channel_id + "> for information on the server.")
+                    # Assemble and send welcome message
+                    msg = self.welcome_msg + ", " + user.mention + ". " + self.about_msg
+                    await self.bot.send_message(def_channel, msg)
 
                 except Exception as e:
                     print("Couldn't finish registration. Registration roles: ")
