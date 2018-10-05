cbb1e1aa5e899a8ebb5dbee7a6d676d6463b23de
chahzou <chahzou.aethyra@gmail.com>
2018-10-05 14:57:08 +0200

    Added message variable to configure info module more easily

--- res/modules/info.py
+++ res/modules/info.py
@@ -6,11 +6,12 @@ from ..module import Module
 class Info(Module):
 
     cmd_arg = 'info'
+    info_msg = ("This is chahzou's discord bot. \nThe code is public: https://github.com/chahzou/discord-bot. " 
+        "Use `help` for info on how to use.\n")
     
     async def run(self, args=None, message=None):
         if message:
-            await self.bot.send_message(message.channel, ("This is chahzou's discord bot. \nThe code is public: https://github.com/chahzou/discord-bot "
-                "Use `help` for info on how to use.\n"))
+            await self.bot.send_message(message.channel, self.info_msg)
 
     async def return_help(self, args=None):
         return "Provides info about the bot."
