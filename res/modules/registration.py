c84ddb75803dfd1abb38a8e06b73052f787d4897
chahzou <chahzou.aethyra@gmail.com>
2018-10-07 12:54:56 +0200

    Improved registration module

--- res/modules/registration.py
+++ res/modules/registration.py
@@ -5,7 +5,7 @@ from ..module import Module
 class Registration(Module):
 
     cmd_arg = 'board'
-    registration_roles = ['on-board', 'passengers']
+    registration_roles = ['registered']
     welcome_msg = "Welcome, "     # Is followed by a user-mention. and the about message.
     about_msg = "See <#497747060880048149> for information on the server."
     auto_delete_cmd = False     # Deactivated because auto-delete is already channel-wide
@@ -19,14 +19,16 @@ class Registration(Module):
                 user = message.author
 
                 try:
-                    for registration_role in self.registration_roles:
-                        role = await self.bot.util.get_role_by_name(message.server, registration_role)
-                        def_channel = await self.bot.util.get_default_channel()
-                        await self.bot.add_roles(user, role)    # Add role
+                    # Add roles
+                    for role_name in self.registration_roles:
+                        role = await self.bot.util.get_role_by_name(message.server, role_name)
+                        await self.bot.add_roles(user, role)
+                        await self.bot.util.print("Gave " + user.name + " the role " + role.name)
                         
                     await self.bot.util.print(user.name + " has been registered.")
 
                     # Assemble and send welcome message
+                    def_channel = await self.bot.util.get_default_channel()
                     msg = self.welcome_msg + user.mention + ". " + self.about_msg
                     await self.bot.send_message(def_channel, msg)
 
@@ -36,8 +38,9 @@ class Registration(Module):
                     print(e)
                     await self.bot.util.send_error_message(message.channel, "Couldn't complete registration.")
 
+                # Delete command for this action
                 if self.auto_delete_cmd:
-                    await self.bot.run_module('mgmt', ['delete', 'last', '1', user.id, 'silent'], message)   # Delete command for this action
+                    await self.bot.run_module('mgmt', ['delete', 'last', '1', user.id, 'silent'], message)
 
 
     async def return_help(self, args=None):
