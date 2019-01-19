dbd2edd0bbd76e2bfb2e541b50ed913c0cda9be9
chahzou <chahzou.aethyra@gmail.com>
2019-01-19 13:32:32 +0100

    Added asyncio loop with restart

--- run.py
+++ run.py
@@ -1,6 +1,21 @@
+import pip, time, asyncio
 from res.bot import Bot
 from res.config import _access
 
-# Runs the bot
+# Install packages
+# pip.main(['install', 'discord'])
+
+
+# Run
+def run_client(client, token):
+    loop = asyncio.get_event_loop()
+    while True:
+        try:
+            loop.run_until_complete(client.start(token))
+        except Exception as e:
+            print("Error", e)
+        time.sleep(30)
+        print("Attempting to restart.")
+
 client = Bot()
-client.run(_access.token)
+run_client(client, _access.token)
