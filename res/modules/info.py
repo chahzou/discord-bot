a989a29c9e7ca416718e89ed07563efab3f5d693
chahzou <chahzou.aethyra@gmail.com>
2019-02-10 21:08:23 +0100

    Improved info message

--- res/modules/info.py
+++ res/modules/info.py
@@ -6,12 +6,13 @@ from ..module import Module
 class Info(Module):
 
     cmd_arg = 'info'
-    info_msg = ("This is chahzou's discord bot. \nThe code is public: https://github.com/chahzou/discord-bot. " 
-        "Use `help` for info on how to use its features.\n")
-    
+
     async def run(self, args=None, message=None):
         if message:
-            await self.bot.send_message(message.channel, self.info_msg)
+            cmd_op = self.bot.cfg.general['cmd_op']
+            info_msg = ("This is chahzou's discord bot. \nThe code is public: https://github.com/chahzou/discord-bot. " 
+                "Use `"+ cmd_op + "help` for info on how to use its features.\n")
+            await self.bot.send_message(message.channel, info_msg)
 
     async def return_help(self, args=None):
         return "Provides info about the bot."
